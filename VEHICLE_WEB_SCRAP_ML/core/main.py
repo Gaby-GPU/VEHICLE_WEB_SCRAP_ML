19aa591c25c1e7598e92ce279d278f2aaad19820
Gaby-GPU <uzzogabriel77@gmail.com>
2024-08-09 17:29:27 -0300

    Add files via upload

---
+++ VEHICLE_WEB_SCRAP_ML/core/main.py
@@ -0,0 +1,10 @@
+from menus.menu import mainMenu
+
+def main():
+    print("Starting the scraping process...")
+    all_data = mainMenu()
+    print(all_data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ VEHICLE_WEB_SCRAP_ML/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Vehicle_Web_Scraping_Project',
+    version='0.1.0',  
+    description='Vehicle web scraping project for data analysis',
+    author='Gabriel Páris Uzzo',
+    author_email='uzzogabriel77@gmail.com',
+    url='https://github.com/Gaby-GPU/VEHICLE_WEB_SCRAP_ML.git',  
+    packages=find_packages(),  
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'mi_proyect=core.main:main',  
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache License 2.0',  
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  
+)
